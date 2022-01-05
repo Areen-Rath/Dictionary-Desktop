816b9c2bef2eacdcea822f620d48ecde83dda7fc
Areen-Rath <lucy_rishi@yahoo.in>
2022-01-05 14:04:19 +0530

    Added comments to code

--- main.js
+++ main.js
@@ -4,14 +4,19 @@ const { app, BrowserWindow } = electron;
 let mainWindow;
 
 app.on('ready', () => {
+    // Create a window but not show as to maximize and open it.
     mainWindow = new BrowserWindow({
         show: false
     });
+
+    // Open maximized window.
     mainWindow.maximize();
     mainWindow.show();
 
+    // Load Dictionary website.
     mainWindow.loadURL("https://areen-rath.github.io/Dictionary");
 
+    // Close app.
     mainWindow.on('closed', () => {
         mainWindow = null;
     });
